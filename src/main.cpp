442adc60a5b0a5810294c0ec87a4602a90d07335
mxr <eetu.kivelae@gmail.com>
2021-04-09 23:49:51 +0300

    Init + appstate struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,50 @@
 #include <raylib-cpp.hpp>
+#include <string>
+
+enum StateTextTypes {
+    FPS,
+    Delta,
+    Elapsed,
+};
+
+struct AppState {
+
+    AppState() {
+        this->update_values();
+    }
+
+    void update_values() {
+        this->fps = GetFPS();
+        this->delta = GetFrameTime();
+        this->elapsed = GetTime();
+    }
+
+    std::string get_text(StateTextTypes type) {
+        std::string text;
+        switch(type) {
+            case FPS:
+                text = std::to_string(this->fps) + " fps";
+                break;
+            case Delta:
+                text = std::to_string(this->delta) + " s";
+                break;
+            case Elapsed:
+                text = std::to_string(this->elapsed) + " seconds";
+                break;
+            default:
+                text = "Invalid value type";
+                break;
+        }
+
+        return text;
+    }
+
+private:
+    int fps;
+    float delta;
+    double elapsed;
+};
+
 
 int main() {
     
@@ -6,11 +52,12 @@ int main() {
     int screenWidth = 800;
     int screenHeight = 450;
 
-    raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    
-    SetTargetFPS(60);
+    AppState app_state = AppState();
 
+    raylib::Color textColor(WHITE);
+    raylib::Window w(screenWidth, screenHeight, "Raylib C++");
+
+    
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
@@ -18,10 +65,14 @@ int main() {
 
         // TODO: Update your variables here
 
+        app_state.update_values();
+
         // Draw
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+        ClearBackground({0, 0, 0, 255});
+        textColor.DrawText(app_state.get_text(FPS), 10, 10, 20);
+        textColor.DrawText(app_state.get_text(Delta), 10, 30, 20);
+        textColor.DrawText(app_state.get_text(Elapsed), 10, 50, 20);
         EndDrawing();
     }
 
